49f46335da6a11d1743d4ada06a67e2f710500ff
jpstroh07 <jan0712.str@gmail.com>
2023-04-01 10:43:31 +0200

    num sum now displayed

--- calculation.js
+++ calculation.js
@@ -1,12 +1,23 @@
 var displayNum = 0.0;
 var firstNum = 0.0;
 var secondNum = 0.0;
+var isSecond = false;
+
+function handleNumber(num) {
+    if (isSecond) {
+        secondNum = numberClick(num);
+    } else {
+        firstNum = numberClick(num);
+    }
+}
 
 function numberClick(num) {
     var old = parseInt(document.getElementById("display").innerHTML) * 10;
 
-    if (old.toString().length != 13) {
-        document.getElementById("display").innerHTML = old + num;
+    if (old.toString().length < 13) {
+        displayNum = old + num;
+        document.getElementById("display").innerHTML = displayNum;
+        return displayNum;
     }
 }
 
@@ -14,6 +25,17 @@ function clearDisplay() {
     displayNum = 0.0;
     firstNum = 0.0;
     secondNum = 0.0;
+    isSecond = false;
 
     document.getElementById("display").innerHTML = displayNum;
+}
+
+function calc() {
+    var b = document.getElementById("sum");
+
+    if (displayNum == 0) {
+        b.innerHTML = b.textContent = "";
+    } else {
+        b.innerHTML = b.textContent = "> " + displayNum;
+    }
 }

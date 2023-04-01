a75d4f2b8ff779ae9f88f86656492f4c2d381ebb
jpstroh07 <jan0712.str@gmail.com>
2023-04-01 09:50:47 +0200

    num correctly displayed when clicked

--- calculation.js
+++ calculation.js
@@ -1,4 +1,11 @@
+var displayNum = 0.0;
+var firstNum = 0.0;
+var secondNum = 0.0;
+
 function numberClick(num) {
-    old = parseInt(document.getElementById("display").innerHTML);
-    document.getElementById("display").innerHTML = old + num;
+    var old = parseInt(document.getElementById("display").innerHTML) * 10;
+
+    if (old.toString().length != 13) {
+        document.getElementById("display").innerHTML = old + num;
+    }
 }
